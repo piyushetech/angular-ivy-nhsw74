1d7ceb00feaa3aebcb4f90b1b7a741be0fbf5792
piyushetech <piyushrana.rana@gmail.com>
2021-11-25 17:12:12 +0530

    2 files modified, 1 deleted

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,45 +6,35 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  /* An empty array that is responsible
-	to add a division */
   public items = [];
-  public items1 = [];
 
-  setData(data:any) {
-    const jsonData = JSON.stringify(data)
-    localStorage.setItem('myData', jsonData)
- }
+  setData(data: any) {
+    const jsonData = JSON.stringify(data);
+    localStorage.setItem('myData', jsonData);
+  }
 
- ngOnInit(){
-  this.items=JSON.parse(this.getData())||[]
- }
- 
- getData() {
-    return localStorage.getItem('myData')
- }
- 
- removeData(key) {
-    localStorage.removeItem(key)
- }
+  ngOnInit() {
+    this.items = JSON.parse(this.getData()) || [];
+  }
 
-  /* A two-way binding performed which
-	pushes text on division */
-  public newTask;
+  getData() {
+    return localStorage.getItem('myData');
+  }
 
-  /* When input is empty, it will
-	not create a new division */
+  removeData(key: string) {
+    localStorage.removeItem(key);
+  }
+
+  public newTask: string;
   public addToList() {
     if (this.newTask == '') {
     } else {
       this.items.push(this.newTask);
       this.newTask = '';
-      this.setData(this.items)
+      this.setData(this.items);
     }
   }
 
-  /* This function takes to input the
-	task, that has to be deleted*/
   public deleteTask(index) {
     this.items.splice(index, 1);
     this.setData(this.items);
